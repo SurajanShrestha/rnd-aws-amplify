59fb337c0c050b31484c8f029c9c07dea28ef91b
Surajan Shrestha <surajanshrestha35@gmail.com>
2023-10-29 10:21:17 +0545

    feat: amplify auth integrated

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "rndawsamplify471a4cc4": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,50 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+// Assets
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
+// Amplify UI
+import "@aws-amplify/ui-react/styles.css";
+import { Button, Image, Authenticator } from "@aws-amplify/ui-react";
+
+// Amplify Backend
+import { Amplify } from "aws-amplify";
+import config from "./aws-exports";
+
+Amplify.configure(config);
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h3>
-        Updated to see if Automatic Code Deployment works in AWS Amplify 🚀
-      </h3>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Learning <code>Vite</code> with <code>React</code>
-        </p>
-      </div>
-    </>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div>
+          <div>
+            <a href="https://vitejs.dev" target="_blank">
+              <Image
+                src={viteLogo}
+                className="logo"
+                height={80}
+                paddingRight={20}
+                alt="Vite logo"
+              />
+            </a>
+            <a href="https://react.dev" target="_blank">
+              <Image
+                src={reactLogo}
+                className="logo"
+                height={80}
+                alt="React logo"
+              />
+            </a>
+          </div>
+          <h1>User: {user?.username}</h1>
+          <Button onClick={signOut} variation="primary">
+            Sign Out
+          </Button>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
